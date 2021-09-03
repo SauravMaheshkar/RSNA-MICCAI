706e923d9f854be09d03165d3bf9b68e389c73d1
SauravMaheshkar <61241031+SauravMaheshkar@users.noreply.github.com>
2021-09-03 09:25:56 +0530

    update model code

--- rsna/nn.py
+++ rsna/nn.py
@@ -6,10 +6,10 @@ __all__ = ["Model"]
 
 
 class Model(nn.Module):
-    def __init__(self):
+    def __init__(self, model_name: str = "efficientnet-b0"):
         super().__init__()
         self.net = EfficientNet3D.from_name(
-            "efficientnet-b0", override_params={"num_classes": 2}, in_channels=1
+            model_name, override_params={"num_classes": 2}, in_channels=1
         )
         n_features = self.net._fc.in_features
         self.net._fc = nn.Linear(in_features=n_features, out_features=1, bias=True)
